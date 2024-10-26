10cf2ae082e8c817d81db8522d1449f229effa83
klenovyi <siropchyk17@gmail.com>
2024-10-26 19:48:02 +0300

    new information in main.c

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ int main() {
     int number;
     printf("DEV1 \n");
     number = 0;
+    // NEW INFORMATION IN FILE
+    printf("DEV2 \n"); 
     printf("Input number to find factorial of this number: ");
     scanf("%d", &number);
 
